83cc95957b6968757c11b65668fb8f2468784742
Baptiste D <bapt.14@hotmail.com>
2025-01-08 00:06:51 +0100

    update 404 not found page

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {NzButtonComponent} from "ng-zorro-antd/button";
 import {RouterLink} from "@angular/router";
+import {NzResultComponent} from "ng-zorro-antd/result";
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
   imports: [
     NzButtonComponent,
-    RouterLink
+    RouterLink,
+    NzResultComponent
   ],
   templateUrl: './not-found.component.html'
 })
