82041eae91d9a1b1705e72e5a792662a48186b4e
Clément Verdoire <123454549+cLemzZz5@users.noreply.github.com>
2025-01-17 14:01:50 +0100

    ✨ 🚧 Works on profil pages (#15)
    * 🧪 mid dev
    
    * ✨ 🚧 Works on profil pages
    
    * 🔥 delete duplocate code
    
    ---------
    
    Co-authored-by: Baptiste D <bapt.14@hotmail.com>

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -1,10 +1,11 @@
-import {Component} from '@angular/core';
-import {HeaderComponent} from "../../components/header/header.component";
-import {UserAuthService} from "../../services/auth/user-auth.service";
-import {User} from "../../model/user.interface";
-import {NgOptimizedImage} from "@angular/common";
-import {NzButtonComponent} from "ng-zorro-antd/button";
-import {PogosButton} from "../../components/pogos-button/pogos-button.component";
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../../components/header/header.component";
+import { UserAuthService } from "../../services/auth/user-auth.service";
+import { User } from "../../model/user.interface";
+import { NgOptimizedImage } from "@angular/common";
+import { PogosButton } from "../../components/pogos-button/pogos-button.component";
+import { RouterLink, RouterOutlet } from "@angular/router";
+import { NzDividerModule } from 'ng-zorro-antd/divider';
 
 @Component({
   selector: 'app-profile-page',
@@ -12,17 +13,19 @@ import {PogosButton} from "../../components/pogos-button/pogos-button.component"
   imports: [
     HeaderComponent,
     NgOptimizedImage,
-    NzButtonComponent,
-    PogosButton
+    PogosButton,
+    RouterOutlet,
+    RouterLink,
+    NzDividerModule
   ],
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.scss'
 })
 export class ProfilePageComponent {
 
-  protected user:User;
+  protected user: User;
 
-  constructor(private readonly userAuthService:UserAuthService) {
+  constructor(private readonly userAuthService: UserAuthService) {
     this.user = this.userAuthService.user;
   }
 
