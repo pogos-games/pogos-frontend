74e8f5d397795ab497464346b5c717b08d9bb76b
Baptiste D <bapt.14@hotmail.com>
2025-05-27 11:40:34 +0200

    continue dev

--- src/app/components/games/uno-table/uno-table.component.ts
+++ src/app/components/games/uno-table/uno-table.component.ts
@@ -1,9 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {UnoHandComponent} from "../uno-hand/uno-hand.component";
 import {UnoCardComponent} from "../uno-card/uno-card.component";
 import {UnoCardBackComponent} from "../uno-card-back/uno-card-back.component";
 import {UnoBackHandComponent} from "../uno-back-hand/uno-back-hand.component";
-import {NzIconDirective} from "ng-zorro-antd/icon";
 
 @Component({
   selector: 'app-uno-table',
@@ -11,8 +10,7 @@ import {NzIconDirective} from "ng-zorro-antd/icon";
     UnoHandComponent,
     UnoCardComponent,
     UnoCardBackComponent,
-    UnoBackHandComponent,
-    NzIconDirective
+    UnoBackHandComponent
   ],
   templateUrl: './uno-table.component.html',
   styleUrl: './uno-table.component.scss'
--- src/app/services/uno.service.ts
+++ src/app/services/uno.service.ts
@@ -1,22 +1,18 @@
-import {inject, Injectable} from '@angular/core';
-import {io, Socket} from "socket.io-client";
-import {ConfigService} from "./config.service";
+import {Injectable} from '@angular/core';
+import {GameService} from "./games/game.service";
 
 @Injectable({
   providedIn: 'root'
 })
-export class UnoService {
-
-  private readonly socket:Socket
-  private readonly configService:ConfigService = inject(ConfigService);
-  private readonly GAMES_URL:string = this.configService.config.GAMES_URL;
+export class UnoService extends GameService{
 
   constructor() {
-      this.socket = io(this.GAMES_URL + '/uno', {
-        path: this.GAMES_URL.startsWith('https') ? '/api/games/socket.io' : '',
-        transports: ['websocket'],
-      });
+    super();
+    this.gameUrl = '/uno';
+    this.initializeSocket();
   }
+
+
 
 
 }
