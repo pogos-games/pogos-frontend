426ca37c87c37c5fce28921b3d859690f9a1a838
clement.verdoire <clem.v08@gmail.com>
2025-05-28 10:23:25 +0200

    🐛 Fix duplication code

--- src/app/components/games/game-table/game-table-blackjack/game-table-blackjack.component.ts
+++ src/app/components/games/game-table/game-table-blackjack/game-table-blackjack.component.ts
@@ -1,38 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { NgOptimizedImage } from "@angular/common";
-import { NzButtonComponent } from "ng-zorro-antd/button";
-import { NzSpinComponent } from "ng-zorro-antd/spin";
-import { GameDeck } from "../../../../model/dto/request/game-deck";
-import { Card } from "../../../../model/dto/request/card";
 import { CardHandComponent } from "../../card-hand/card-hand.component";
-import { GameActions } from "../../../../model/enum/game.actions.enum";
+import { GameTableComponent } from '../game-table.component';
 
 @Component({
   selector: 'app-game-table',
   imports: [
     NgOptimizedImage,
-    NzButtonComponent,
-    NzSpinComponent,
     CardHandComponent
   ],
   templateUrl: './game-table-blackjack.component.html',
   standalone: true,
   styleUrl: './game-table-blackjack.component.scss'
 })
-export class GameTableBlackjackComponent {
-  protected gameDeck: GameDeck = { playerHand: new Set<Card>(), dealerHand: new Set<Card>(), playerTotal: 0, message: "CONTINUE" };
-  @Input()
-  public hands!: {
-    player1Hand: Card[],
-    player2Hand: Card[],
-    player3Hand: Card[],
-    dealerHand: Card[],
-    selfHand: Card[]
-  };
-
-  executeAction(action: GameActions): void {
-    console.log('action : ', action)
-  }
-
-  protected readonly GameActions = GameActions;
+export class GameTableBlackjackComponent extends GameTableComponent {
 }
---
+++ src/app/components/games/game-table/game-table.component.ts
@@ -0,0 +1,23 @@
+import { Directive, Input } from '@angular/core';
+import { GameDeck } from '../../../model/dto/request/game-deck';
+import { Card } from '../../../model/dto/request/card';
+import { GameActions } from '../../../model/enum/game.actions.enum';
+
+@Directive()
+export class GameTableComponent {
+  protected gameDeck: GameDeck = { playerHand: new Set<Card>(), dealerHand: new Set<Card>(), playerTotal: 0, message: "CONTINUE" };
+  @Input()
+  public hands!: {
+    player1Hand: Card[],
+    player2Hand: Card[],
+    player3Hand: Card[],
+    dealerHand: Card[],
+    selfHand: Card[]
+  };
+
+  executeAction(action: GameActions): void {
+    console.log('action : ', action)
+  }
+
+  protected readonly GameActions = GameActions;
+}
