e081a0661397e562a8113edaccf69dd6accf14d2
clement.verdoire <clem.v08@gmail.com>
2025-05-28 10:24:39 +0200

    🐛

--- src/app/components/games/game-table/game-table-poker/game-table-poker.component.ts
+++ src/app/components/games/game-table/game-table-poker/game-table-poker.component.ts
@@ -1,11 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { NgOptimizedImage } from "@angular/common";
 import { NzButtonComponent } from "ng-zorro-antd/button";
 import { NzSpinComponent } from "ng-zorro-antd/spin";
-import { GameDeck } from "../../../../model/dto/request/game-deck";
-import { Card } from "../../../../model/dto/request/card";
 import { CardHandComponent } from "../../card-hand/card-hand.component";
-import { GameActions } from "../../../../model/enum/game.actions.enum";
 import { GameTableComponent } from '../game-table.component';
 
 @Component({
