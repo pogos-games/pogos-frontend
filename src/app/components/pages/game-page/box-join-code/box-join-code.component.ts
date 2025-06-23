5f0cecfcabbba563a517a4ff7818eb66ab4648fa
Baptiste D <bapt.14@hotmail.com>
2025-06-23 16:45:34 +0200

    🎨 show notification when error on box join code

--- src/app/components/pages/game-page/box-join-code/box-join-code.component.ts
+++ src/app/components/pages/game-page/box-join-code/box-join-code.component.ts
@@ -1,6 +1,7 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, inject, Output} from '@angular/core';
 import {NzInputDirective} from "ng-zorro-antd/input";
 import {FormsModule} from "@angular/forms";
+import {NzNotificationService} from "ng-zorro-antd/notification";
 
 @Component({
   selector: 'app-box-join-code',
@@ -15,11 +16,22 @@ import {FormsModule} from "@angular/forms";
 export class BoxJoinCodeComponent {
   @Output() searchGameEvent: EventEmitter<string> = new EventEmitter<string>();
 
+  private readonly nzNotificationService : NzNotificationService = inject(NzNotificationService);
+
   joinCode: string = ""
   searchGame() {
-    const code = this.joinCode.trim();
-    if (code) {
+    const code = this.joinCode.trim().replace(/^#/, '');
+    // remove # from code
+    if (code && code.length === 4) {
       this.searchGameEvent.emit(code);
-    }
-  }
+    } else{
+      console.log('erreur')
+        this.nzNotificationService.create('error', 'Erreur', 'Le code saisi est invalide (ex: #4849)', {
+          nzClass: 'custom-notification',
+          nzDuration: 5000
+        });
+      }
+}
+
+
 }
